b19bceea6d8176db34f3b286b259c4537b36f417
ADMIN-00007 <140091643+ADMIN-00007@users.noreply.github.com>
2023-07-31 14:45:34 +0200

    Second commit

--- OrangeApp/OrangeDashBoard/Form1.Designer.cs
+++ OrangeApp/OrangeDashBoard/Form1.Designer.cs
@@ -28,43 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(338, 101);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 34);
-            textBox1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(336, 68);
+            label1.Location = new Point(215, 99);
             label1.Name = "label1";
-            label1.Size = new Size(126, 28);
+            label1.Size = new Size(167, 28);
             label1.TabIndex = 1;
-            label1.Text = "Version Org3";
+            label1.Text = "Orange Version  8";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.Brightkite_high_detail_256;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(261, 177);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(174, 174);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(301, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 28);
+            label2.TabIndex = 3;
+            label2.Text = "B2B Form";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 28F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1453, 608);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
-            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Orange Dashboard";
             WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
     }
 }
--- OrangeApp/OrangeDashBoard/Form1.cs
+++ OrangeApp/OrangeDashBoard/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace OrangeDashBoard
 {
     public partial class Form1 : Form
@@ -5,6 +7,11 @@ namespace OrangeDashBoard
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = @"https://forms.office.com/Pages/ResponsePage.aspx?id=Uxo8h1TYokeM71lH3Fj_sdQBH5zizjBImbSQ_pb3sepURExWQkY2VzlVTlhWNFZERUxQVzZGMTA0RS4u", UseShellExecute = true });
         }
     }
 }
